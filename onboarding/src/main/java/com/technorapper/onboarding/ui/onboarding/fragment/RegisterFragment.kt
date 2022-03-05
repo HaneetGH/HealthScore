6e29f22bd922de6f6ec46d6472e92b5f6338e455
Haneet Singh Chhabra <haneet555@gmail.com>
2022-03-05 23:51:13 +0530

    register user in firestore

--- onboarding/src/main/java/com/technorapper/onboarding/ui/onboarding/fragment/RegisterFragment.kt
+++ onboarding/src/main/java/com/technorapper/onboarding/ui/onboarding/fragment/RegisterFragment.kt
@@ -3,6 +3,7 @@ package com.technorapper.onboarding.ui.onboarding.fragment
 
 import com.technorapper.root.ui.list.RootActivity
 import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -15,6 +16,8 @@ import androidx.lifecycle.Observer
 import com.google.firebase.FirebaseException
 import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.technorapper.onboarding.R
 import com.technorapper.onboarding.base.BaseFragment
 import com.technorapper.onboarding.constant.Task
@@ -87,14 +90,41 @@ class RegisterFragment : BaseFragment() {
                         when (it.task) {
                             Task.ONBOARD -> {
                                 try {
-                                    val value = it.data as com.google.android.gms.tasks.Task<AuthResult>
+                                    val value =
+                                        it.data as com.google.android.gms.tasks.Task<AuthResult>
                                     value.addOnCompleteListener {
-                                        if (it.isSuccessful)
-                                        {
-                                            startActivity(Intent(activity, RootActivity::class.java))
-                                        }
-                                        else
-                                            binding.isOTPGenerated=false
+                                        if (it.isSuccessful) {
+                                            val db = Firebase.firestore
+
+                                            // Create a new user with a first and last name
+                                            val user = hashMapOf(
+                                                "userDOB" to "Ada",
+                                                "userID" to it.result.user?.uid,
+                                                "userLastLocation" to 1815,
+                                                "userName" to "1,1",
+                                                "userPhone" to 9041422652
+                                            )
+
+// Add a new document with a generated ID
+                                            db.collection("users")
+                                                .add(user)
+                                                .addOnSuccessListener { documentReference ->
+                                                    Log.d(
+                                                        TAG,
+                                                        "DocumentSnapshot added with ID: ${documentReference.id}"
+                                                    )
+                                                }
+                                                .addOnFailureListener { e ->
+                                                    Log.w(TAG, "Error adding document", e)
+                                                }
+                                            startActivity(
+                                                Intent(
+                                                    activity,
+                                                    RootActivity::class.java
+                                                )
+                                            )
+                                        } else
+                                            binding.isOTPGenerated = false
 
                                     }
 
--- root/src/main/java/com/technorapper/root/ui/list/RootActivity.kt
+++ root/src/main/java/com/technorapper/root/ui/list/RootActivity.kt
@@ -1,9 +1,13 @@
 package com.technorapper.root.ui.list
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 import com.technorapper.root.R
 import com.technorapper.root.base.BaseClass
@@ -21,7 +25,17 @@ class RootActivity : BaseClass() {
     override fun setBinding() {
         binding = DataBindingUtil.setContentView(this, R.layout.root_activity)
         binding.bottomNavigation.setupWithNavController(Navigation.findNavController(this, R.id.fragmentContainerView))
+        val db = Firebase.firestore
+        db.collection("users")
+            .whereEqualTo("userID", 1)
+            .addSnapshotListener { snapshots, e ->
+                if (e != null) {
+                    Log.w(TAG, "Listen failed.", e)
+                    return@addSnapshotListener
+                }
 
+                Log.d(TAG, "Current users : ${snapshots!!.documents[0].data?.get("userName")}")
+            }
     }
 
 
