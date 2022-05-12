2e5aa45b7c01e83b5582853fb44103c2dc41bffb
Haneet Singh Chhabra <haneet555@gmail.com>
2022-05-13 01:27:03 +0530

    dismiss correct

--- root/src/main/java/com/technorapper/root/ui/components/CustomAlertDialog.kt
+++ root/src/main/java/com/technorapper/root/ui/components/CustomAlertDialog.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -46,7 +47,6 @@ class Alerts {
                         contentAlignment = Alignment.Center
                     ) {
                         Column(modifier = Modifier.padding(20.dp)) {
-
                             Row(
                                 modifier = Modifier.fillMaxWidth(),
                                 horizontalArrangement = Arrangement.SpaceBetween,
